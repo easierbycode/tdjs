f743c5dc89c7b72ab9a753218a9d99ef081edf52
Daniel Johnson <navseeker7@gmail.com>
2012-08-13 18:49:09 -0700

    lint all .js files

--- jakefile.js
+++ jakefile.js
@@ -1,9 +1,37 @@
+/*global desc, task, jake, fail, complete */
+
+"use strict";
+
 task('default', ['lint']);
 
 desc('lint everything');
 task('lint', [], function() {
-    var lint = require('./build/lint/lint_runner.js');
-    lint.validateFile('jakefile.js', {}, {});
+    var lint = require("./build/lint/lint_runner.js");
+
+    var files = new jake.FileList();
+    files.include("**/*.js");
+    files.exclude("node_modules");
+    files.exclude("build");
+
+    var options = {
+        bitwise: true,
+        curly: false,
+        eqeqeq: true,
+        forin: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        noempty: true,
+        nonew: true,
+        regexp: true,
+        undef: true,
+        strict: true,
+        trailing: true,
+        node: true
+    };
+
+    lint.validateFileList(files.toArray(), options, {});
 });
 
 desc('Example!');
