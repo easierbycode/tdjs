70a04281089524645ee805d812851e9e57a60815
Daniel Johnson <daniel.johnson@evogi.com>
2012-12-01 22:24:42 -0700

    finished ep4

--- jakefile.js
+++ jakefile.js
@@ -1,51 +1,56 @@
 /*global desc, task, jake, fail, complete */
+(function() {
+  "use strict";
 
-"use strict";
+  desc("Build and test");
 
-desc("Build and test");
+  task('default', ['lint']);
 
-task('default', ['lint']);
+  desc('lint everything');
+  task('lint', [], function() {
+      var lint = require("./build/lint/lint_runner.js");
 
-desc('lint everything');
-task('lint', [], function() {
-    var lint = require("./build/lint/lint_runner.js");
+      var files = new jake.FileList();
+      files.include("**/*.js");
+      files.exclude("node_modules");
 
-    var files = new jake.FileList();
-    files.include("**/*.js");
-    files.exclude("node_modules");
-    files.exclude("build");
+      var options = nodeLintOptions();
+      var passed = lint.validateFileList(files.toArray(), options, {});
 
-    var options = {
-        bitwise: true,
-        curly: false,
-        eqeqeq: true,
-        forin: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        noempty: true,
-        nonew: true,
-        regexp: true,
-        undef: true,
-        strict: true,
-        trailing: true,
-        node: true
+      if(!passed) fail('Lint failed');
+  });
+
+  function nodeLintOptions() {
+    return {
+      bitwise: true,
+      curly: false,
+      eqeqeq: true,
+      forin: true,
+      immed: true,
+      latedef: true,
+      newcap: true,
+      noarg: true,
+      noempty: true,
+      nonew: true,
+      regexp: true,
+      undef: true,
+      strict: true,
+      trailing: true,
+      node: true
     };
+  }
 
-    lint.validateFileList(files.toArray(), options, {});
-});
+  desc("Integrate");
+  task("integrate", ["default"], function() {
+      console.log("Integration logic goes here");
+  });
 
-desc("Integrate");
-task("integrate", ["default"], function() {
-    console.log("Integration logic goes here");
-});
+  desc('Example!');
+  task("example", ["dependency"], function() {
+      console.log('Example!');
+  });
 
-desc('Example!');
-task("example", ["dependency"], function() {
-    console.log('Example!');
-});
-
-task("dependency", function() {
-    console.log('Dependency!');
-});
+  task("dependency", function() {
+      console.log('Dependency!');
+  });
+}());
